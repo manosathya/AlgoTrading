079246cb33fd183a57359ebb020b2d47c83508dc
manosathya <manosathya98@gmail.com>
2025-04-30 16:27:28 +0100

    Added argparse inputs to run_strats.py

--- run_strats.py
+++ run_strats.py
@@ -5,12 +5,23 @@ import json
 import asyncio
 import redis.asyncio as redis
 
+import argparse
+
 redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 strategy_configs = load_yaml_config('configs/strategy_configs.yaml')
-strategy_name = 'RSI'
-mode = 'test'
-submit_orders = False
-plot_type = 'dash'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--strategy', type=str, required=True, help='Name of strategy class (e.g., RSI)')
+parser.add_argument('--mode', type=str, choices=['paper', 'backtest', 'test'], default='test')
+parser.add_argument('--submit_orders', action='store_true')
+parser.add_argument('--plot_type', type=str, choices=['dash', 'jupyter', 'None'], default='dash')
+
+args = parser.parse_args()
+
+strategy_name = args.strategy
+mode = args.mode
+submit_orders = args.submit_orders
+plot_type = None if args.plot_type == 'None' else args.plot_type
 
 async def run_strategy():
     try:
